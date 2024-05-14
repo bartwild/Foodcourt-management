d1bd1d9a3ec071b7d346fcbddd871c482a288b3d
kmieleck <kasia.mielecka8@gmail.com>
2024-05-14 23:54:05 +0200

    restaurants view without restaurants

--- app/models.py
+++ app/models.py
@@ -1,8 +1,18 @@
-from .database import db
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(200))
+    description = db.Column(db.String(500), nullable=True)  # Dodanie pola opisu
+
+    products = db.relationship('Product', backref='restaurant', lazy=True)
 
 class Product(db.Model):
-    __tablename__ = "product"
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    image_url = db.Column(db.String(200))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
