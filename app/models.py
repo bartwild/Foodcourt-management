daf8c1a9790bcd4986d777d8699f4397b96b4e14
Julia Macuga <julia.macuga@gmail.com>
2024-05-15 20:39:42 +0200

    initial migradion, tables names added

--- app/models.py
+++ app/models.py
@@ -2,17 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 from . import db
 
+
 class Restaurant(db.Model):
+    __tablename__ = "restaurant"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     image_url = db.Column(db.String(200))
     description = db.Column(db.String(500), nullable=True)
 
-    products = db.relationship('Product', backref='restaurant', lazy=True)
+    products = db.relationship("Product", backref="restaurant", lazy=True)
+
 
 class Product(db.Model):
+    __tablename__ = "product"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text)
     image_url = db.Column(db.String(200))
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    restaurant_id = db.Column(
+        db.Integer, db.ForeignKey("restaurant.id"), nullable=False
+    )
