3401de6ec1405e56cec50e636e691b2476ab8c44
patrykploskipw <01168908@pw.edu.pl>
2024-05-20 20:15:35 +0200

    food_ready

--- app/static/food_ready.js
+++ app/static/food_ready.js
@@ -1,17 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const notification = document.getElementById('notification');
-    const closeBtn = document.getElementById('close-btn');
     const pickupBtn = document.getElementById('pickup-btn');
 
-    closeBtn.addEventListener('click', () => {
-        notification.classList.remove('show');
-    });
-
     pickupBtn.addEventListener('click', () => {
-        notification.classList.add('show');
         setTimeout(() => {
-            notification.classList.remove('show');
-            // You can add any additional action here after the notification hides
-        }, 5000); // Hide notification after 5 seconds
+            window.location.href = '/timer'; // Redirect to the timer page after 10 seconds
+        }, 10000); // 10 seconds delay
     });
 });
