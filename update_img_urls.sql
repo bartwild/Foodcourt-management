08806ebfc9e602eec383d32b1db11444cb5f9377
Julia Macuga <julia.macuga@gmail.com>
2024-06-03 23:45:52 +0200

    update database sql

---
+++ update_img_urls.sql
@@ -0,0 +1,3 @@
+update restaurants set image_url='images/food/pizzeria.jpg' where id=1;
+update restaurants set image_url='images/food/grillcraft.jpg' where id=3;
+update restaurants set image_url='images/food/burgerownia.jpg' where id=2;
